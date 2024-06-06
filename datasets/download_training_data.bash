01aa24b83291bacc86a2244c474c7c3a1a0e8afb
keyser <PYL35@hotmail.fr>
2024-06-06 16:42:34 +0200

    Update download_training_data.bash

--- datasets/download_training_data.bash
+++ datasets/download_training_data.bash
@@ -11,57 +11,75 @@ mkdir -p MD_DA/only_protein
 mkdir -p reduced
 
 wget https://zenodo.org/record/10390550/files/4D_complex_training_set.zip?download=1 -O 4D_data/data.zip
-unzip 4D_data/data.zip 4D_data/training_set
+unzip 4D_data/data.zip -d 4D_data
+rm 4D_data/data.zip
 
 wget https://zenodo.org/record/10390550/files/4D_complex_validation_set.zip?download=1 -O 4D_data/data.zip
-unzip 4D_data/data.zip 4D_data/validation_set
+unzip 4D_data/data.zip -d 4D_data
+rm 4D_data/data.zip
 
 wget https://zenodo.org/record/10390550/files/4D_only_ligand_training_set.zip?download=1 -O 4D_data_only_ligand/data.zip
-unzip 4D_data_only_ligand/data.zip 4D_data_only_ligand/training_set
+unzip 4D_data_only_ligand/data.zip -d 4D_data_only_ligand
+rm 4D_data_only_ligand/data.zip
 
 wget https://zenodo.org/record/10390550/files/4D_only_ligand_validation_set.zip?download=1 -O 4D_data_only_ligand/data.zip
-unzip 4D_data_only_ligand/data.zip 4D_data_only_ligand/validation_set
+unzip 4D_data_only_ligand/data.zip -d 4D_data_only_ligand
+rm 4D_data_only_ligand/data.zip
 
 wget https://zenodo.org/record/10390550/files/4D_only_ligand_tracking_training_set.zip?download=1 -O 4D_data_only_ligand_tracking/data.zip
-unzip 4D_data_only_ligand_tracking/data.zip 4D_data_only_ligand_tracking/training_set
+unzip 4D_data_only_ligand_tracking/data.zip -d 4D_data_only_ligand_tracking
+rm 4D_data_only_ligand_tracking/data.zip
 
 wget https://zenodo.org/record/10390550/files/4D_only_ligand_tracking_validation_set.zip?download=1 -O 4D_data_only_ligand_tracking/data.zip
-unzip 4D_data_only_ligand_tracking/data.zip 4D_data_only_ligand_tracking/validation_set
+unzip 4D_data_only_ligand_tracking/data.zip -d 4D_data_only_ligand_tracking
+rm 4D_data_only_ligand_tracking/data.zip
 
 wget https://zenodo.org/record/10390550/files/4D_only_protein_training_set.zip?download=1 -O 4D_data_only_protein/data.zip
-unzip 4D_data_only_protein/data.zip 4D_data_only_protein/training_set
+unzip 4D_data_only_protein/data.zip -d 4D_data_only_protein
+rm 4D_data_only_protein/data.zip
 
 wget https://zenodo.org/record/10390550/files/4D_only_protein_validation_set.zip?download=1 -O 4D_data_only_protein/data.zip
-unzip 4D_data_only_protein/data.zip 4D_data_only_protein/validation_set
+unzip 4D_data_only_protein/data.zip -d 4D_data_only_protein
+rm 4D_data_only_protein/data.zip
 
 wget https://zenodo.org/record/10390550/files/initial_training_set.zip?download=1 -O no_MD/data.zip
-unzip no_MD/data.zip no_MD/training_set.hdf
+unzip no_MD/data.zip -d no_MD
+rm no_MD/data.zip
 
 wget https://zenodo.org/record/10390550/files/initial_validation_set.zip?download=1 -O no_MD/data.zip
-unzip no_MD/data.zip no_MD/validation_set.hdf
+unzip no_MD/data.zip -d no_MD
+rm no_MD/data.zip
 
 wget https://zenodo.org/record/10390550/files/MDDA_complex_training_set_augmented.zip?download=1 -O MD_DA/complex/data.zip
-unzip MD_DA/complex/data.zip MD_DA/complex/training_set.hdf
+unzip MD_DA/complex/data.zip -d MD_DA/complex
+rm MD_DA/complex/data.zip
 
 wget https://zenodo.org/record/10390550/files/MDDA_complex_validation_set_augmented.zip?download=1 -O MD_DA/complex/data.zip
-unzip MD_DA/complex/data.zip MD_DA/complex/validation_set.hdf
+unzip MD_DA/complex/data.zip -d MD_DA/complex
+rm MD_DA/complex/data.zip
 
 wget https://zenodo.org/record/10390550/files/MDDA_only_ligand_training_set_augmented.zip?download=1 -O MD_DA/only_ligand/data.zip
-unzip MD_DA/only_ligand/data.zip MD_DA/only_ligand/training_set.hdf
+unzip MD_DA/only_ligand/data.zip -d MD_DA/only_ligand
+rm MD_DA/only_ligand/data.zip
 
 wget https://zenodo.org/record/10390550/files/MDDA_only_ligand_validation_set_augmented.zip?download=1 -O MD_DA/only_ligand/data.zip
-unzip MD_DA/only_ligand/data.zip MD_DA/only_ligand/validation_set.hdf
+unzip MD_DA/only_ligand/data.zip -d MD_DA/only_ligand
+rm MD_DA/only_ligand/data.zip
 
 wget https://zenodo.org/record/10390550/files/MDDA_only_protein_training_set_augmented.zip?download=1 -O MD_DA/only_protein/data.zip
-unzip MD_DA/only_protein/data.zip MD_DA/only_protein/training_set.hdf
+unzip MD_DA/only_protein/data.zip -d MD_DA/only_protein
+rm MD_DA/only_protein/data.zip
 
 wget https://zenodo.org/record/10390550/files/MDDA_only_protein_validation_set_augmented.zip?download=1 -O MD_DA/only_protein/data.zip
-unzip MD_DA/only_protein/data.zip MD_DA/only_protein/validation_set.hdf
+unzip MD_DA/only_protein/data.zip -d MD_DA/only_protein
+rm MD_DA/only_protein/data.zip
 
 wget https://zenodo.org/record/10390550/files/reduced_training_set.zip?download=1 -O reduced/data.zip
-unzip reduced/data.zip reduced/training_set.hdf
+unzip reduced/data.zip -d reduced
+rm reduced/data.zip
 
 wget https://zenodo.org/record/10390550/files/reduced_validation_set.zip?download=1 -O reduced/data.zip
-unzip reduced/data.zip reduced/validation_set.hdf
+unzip reduced/data.zip -d reduced
+rm reduced/data.zip
 
 echo "Downloaded and unpacked training/validation sets"
