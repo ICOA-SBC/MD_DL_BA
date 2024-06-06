72e672fbebb86924d5d88a0bf21f97abd12db309
keyser <PYL35@hotmail.fr>
2024-06-06 15:33:39 +0200

    Update download_test_data.bash

--- datasets/download_test_data.bash
+++ datasets/download_test_data.bash
@@ -12,33 +12,43 @@ mkdir -p reduced
 mkdir -p fep
 
 wget https://zenodo.org/record/10390550/files/4D_complex_test_set.zip?download=1 -O 4D_data/data.zip
-unzip 4D_data/data.zip 4D_data/test_set
+unzip 4D_data/data.zip -d 4D_data
+rm 4D_data/data.zip
 
 wget https://zenodo.org/record/10390550/files/4D_only_ligand_test_set.zip?download=1 -O 4D_data_only_ligand/data.zip
-unzip 4D_data_only_ligand/data.zip 4D_data_only_ligand/test_set
+unzip 4D_data_only_ligand/data.zip -d 4D_data_only_ligand
+rm 4D_data_only_ligand/data.zip
 
 wget https://zenodo.org/record/10390550/files/4D_only_ligand_tracking_test_set.zip?download=1 -O 4D_data_only_ligand_tracking/data.zip
-unzip 4D_data_only_ligand_tracking/data.zip 4D_data_only_ligand_tracking/test_set
+unzip 4D_data_only_ligand_tracking/data.zip -d 4D_data_only_ligand_tracking
+rm 4D_data_only_ligand_tracking/data.zip
 
 wget https://zenodo.org/record/10390550/files/4D_only_protein_test_set.zip?download=1 -O 4D_data_only_protein/data.zip
-unzip 4D_data_only_protein/data.zip 4D_data_only_protein/test_set
+unzip 4D_data_only_protein/data.zip -d 4D_data_only_protein
+rm 4D_data_only_protein/data.zip
 
 wget https://zenodo.org/record/10390550/files/initial_test_set.zip?download=1 -O no_MD/data.zip
-unzip no_MD/data.zip no_MD/test_set.hdf
+unzip no_MD/data.zip -d no_MD
+rm no_MD/data.zip
 
 wget https://zenodo.org/record/10390550/files/MDDA_complex_test_set_augmented.zip?download=1 -O MD_DA/complex/data.zip
-unzip MD_DA/complex/data.zip MD_DA/complex/test_set.hdf
+unzip MD_DA/complex/data.zip -d MD_DA/complex
+rm MD_DA/complex/data.zip
 
 wget https://zenodo.org/record/10390550/files/MDDA_only_ligand_test_set_augmented.zip?download=1 -O MD_DA/only_ligand/data.zip
-unzip MD_DA/only_ligand/data.zip MD_DA/only_ligand/test_set.hdf
+unzip MD_DA/only_ligand/data.zip -d MD_DA/only_ligand
+rm MD_DA/only_ligand/data.zip
 
 wget https://zenodo.org/record/10390550/files/MDDA_only_protein_test_set_augmented.zip?download=1 -O MD_DA/only_protein/data.zip
-unzip MD_DA/only_protein/data.zip MD_DA/only_protein/test_set.hdf
+unzip MD_DA/only_protein/data.zip -d MD_DA/only_protein
+rm MD_DA/only_protein/data.zip
 
 wget https://zenodo.org/record/10390550/files/reduced_test_set.zip?download=1 -O reduced/data.zip
-unzip reduced/data.zip reduced/test_set.hdf
+unzip reduced/data.zip -d reduced
+rm reduced/data.zip
 
 wget https://zenodo.org/record/10390550/files/fep_test_set.zip?download=1 -O fep/data.zip
-unzip fep/data.zip fep/fep.hdf
+unzip fep/data.zip -d fep
+rm fep/data.zip
 
 echo "Downloaded and unpacked test sets"
